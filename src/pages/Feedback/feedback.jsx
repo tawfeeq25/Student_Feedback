16190f262dbfc6b9ede686cee6ca5f1ab29b1465
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2023-04-16 22:09:42 +0530

    feedback page structure completed

--- src/pages/Feedback/feedback.jsx
+++ src/pages/Feedback/feedback.jsx
@@ -1,37 +1,55 @@
-import '../../styles/feed.css';
-const Qs=()=>{
+import '../../styles/feed.css';//importing css for this page
+//feedback page code
+const question=['Question 1','Question 2','Question 3','Question 4','Question 5'];//set of question for feedback
+const subjects=['Sub1','Sub2','Sub3','Sub4','Sub5'];//set of subjects
+var count=0;
+const Choice=(props)=>{
     return(<>
-         <div id="ques">
-                <div id="quest">
-                <p>Rate for attendance of staff</p>
-                </div>
-                <div id="mark">
-                <div id="choice">
-		        <div id="markk"><span class="one">1</span><span class="two">2</span><span class="three">3</span><span class="four">4</span><span class="five">5</span></div><br/>
+		       
 		        <div id="ch">
-		        <input type="radio"  id="choi" name="val" value="1"  required />
+                <div id="sub">{props.name}</div>
+                <div id="choi">
+                <input type="radio"  id="choi" name="val"  value="1"  required />
 		        <input type="radio"   id="choi" name="val" value="2" />
 		        <input type="radio"   id="choi" name="val" value="3" checked/>
 		        <input type="radio"   id="choi" name="val" value="4" />
 		        <input type="radio"   id="choi" name="val" value="5"  />
+                </div>
+		        
 		            </div>
-		        </div>
+		       
+    </>)
+}
+//question jsx component
+function Choi(){return (
+    subjects.map((i)=><><Choice name={i}/></>)
+    )}
+const Qs=(props)=>
+{
+    count=count+1;
+    return(<>
+         <div id="ques">
+                <div id="quest">
+                <p>{count}.{props.name}</p>
+                </div>
+                <div id="choice">
+                <div id="mark">
+                <div id="markk"><span class="one">1</span><span class="two">2</span><span class="three">3</span><span class="four">4</span><span class="five">5</span></div><br/>
+               <Choi/>
+                </div>
                 </div>
             </div>
     </>)
 }
+function Ques(){return (
+    question.map((i)=><><Qs name={i}/></>)
+    )}
+//feedback jsx component
 const Feed=()=>{
     return(<>
     <div id="feedcont" className="box">
         <div id="feedques">
-            <Qs/>
-            <Qs/>
-            <Qs/>
-            <Qs/>
-            <Qs/>
-            <Qs/> <Qs/>
-            <Qs/>
-            <Qs/>
+            <Ques/>
         </div>
     </div>
     </>)
