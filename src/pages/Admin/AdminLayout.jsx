e10f26f1f0329a7d465ac8e143e048bc8754e278
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2023-04-22 23:26:20 +0530

    admin page staff table design

--- src/pages/Admin/AdminLayout.jsx
+++ src/pages/Admin/AdminLayout.jsx
@@ -37,13 +37,47 @@ const AdminLayout=()=>{
             <div id="admin-cont">
                     <div id="dash-inner">
                             <div id="dash-nav">
+                                <div id="sec-a">SEC-A</div>
+                                <div id="sec-b">SEC-B</div>
 
                             </div>
                             <div id="dash-cont">
                                 <div id="dash-cont-staff">
                                     <div id="dash-cont-st">
                                         <div id="staff-list">
+                                            <table cellSpacing="0" cellPadding="0" id="staff-table">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Subjects</th>
+                                                        <th>Staff</th>
+                                                        <th>Points</th>
+                                                    </tr>
 
+                                                </thead>
+                                                <tbody>
+                                                <tr>
+                                               
+                                                <td>6,000</td>
+                                                <td>110</td>
+                                                <td>1.8%</td>
+                                               
+                                                </tr>
+                                                <tr>
+                                               
+                                                <td>2,200</td>
+                                                <td>500</td>
+                                                <td>22%</td>
+                                               
+                                                </tr>
+                                                <tr>
+                                               
+                                                <td>13,500</td>
+                                                <td>900</td>
+                                                <td>6.7%</td>
+                                                
+                                                 </tr>
+                                                </tbody>
+                                            </table>
                                         </div>
                                     </div>
                                     <div id="dash-count">
